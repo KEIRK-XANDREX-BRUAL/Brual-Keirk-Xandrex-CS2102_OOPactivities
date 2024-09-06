706428219bf36b4b935f25a0c9e322d495d2dacc
KEIRK-XANDREX-BRUAL <23-02544@g.batstate-u.edu.ph>
2024-09-06 14:55:26 +0800

    Add files via upload

---
+++ Brual_Keirk_Xandrex_CS-2102_OOPactivities/Exercise Sets/Escseq.java
@@ -0,0 +1,12 @@
+class Escseq {
+    public static void main(String[] args) {
+    for (int x = 1; x < 6; x++){
+        if (x % 2 == 0){
+            System.out.println("\t" + (x * 7));
+        }
+        else {
+            System.out.println(x * 7);
+        }
+        }
+    }
+}
